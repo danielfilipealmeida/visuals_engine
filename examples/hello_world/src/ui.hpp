66c7d2e8953d8d4d3a6f5ed2d5055a0cc7d8dfad
Daniel Almeida <danielf.almeida@gmail.com>
2025-07-10 01:02:48 +0100

    chore: split elements into several panels

--- examples/hello_world/src/ui.hpp
+++ examples/hello_world/src/ui.hpp
@@ -15,12 +15,14 @@
 
 class ui {
     shared_ptr<State> state;
-    ofxPanel gui;
+    ofxPanel previewsPanel, audiAndFFTPanel, mainOptionsPanel;
     ofParameter<float> blur;
     ofParameter<float> mix;
     
     std::vector<std::pair<string, VisualsInterface *>> additionalVisuals;
 
+    float panelsWidth = 256;
+    float panelsMargin = 8;
 public:
     ui() {};
     
@@ -28,24 +30,31 @@ public:
         this->state = state;
         this->additionalVisuals = additionalVisuals;
         
-        gui.setup();
-        
         // setup mixer and channels previews
-        gui.add(new DrawableGuiElement<Mixer *>(this->state->mixer, "Mixer"));
-        gui.add(new DrawableGuiElement<VisualsInterface *>(this->state->mixer->a, "Channel A"));
-        gui.add(new DrawableGuiElement<VisualsInterface *>(this->state->mixer->b, "Channel B"));
+        previewsPanel.setup("Previews", "previewPanelConfig");
+        previewsPanel.setWidthElements(panelsWidth);
+        previewsPanel.setPosition(panelsMargin, panelsMargin);
+        previewsPanel.add(new DrawableGuiElement<Mixer *>(this->state->mixer, "Mixer"));
+        previewsPanel.add(new DrawableGuiElement<VisualsInterface *>(this->state->mixer->a, "Channel A"));
+        previewsPanel.add(new DrawableGuiElement<VisualsInterface *>(this->state->mixer->b, "Channel B"));
         
         // add additional plots
+        audiAndFFTPanel.setup("Audio and FFT", "audioAndFFTConfig");
+        audiAndFFTPanel.setWidthElements(panelsWidth);
+        audiAndFFTPanel.setPosition(panelsMargin*2 + panelsWidth, panelsMargin);
         for(std::pair<string, VisualsInterface *> plot : this->additionalVisuals) {
-            gui.add(new DrawableGuiElement<VisualsInterface *>(plot.second, plot.first));
+            audiAndFFTPanel.add(new DrawableGuiElement<VisualsInterface *>(plot.second, plot.first));
         }
         
         
         // setup. needs a listener to update the state
-        gui.add(blur.set("blur", 0, 0, 10));
+        mainOptionsPanel.setup("Attributes", "attributesConfig");
+        mainOptionsPanel.setWidthElements(panelsWidth);
+        mainOptionsPanel.setPosition(panelsMargin*3 + panelsWidth * 2, panelsMargin);
+        mainOptionsPanel.add(blur.set("blur", 0, 0, 10));
         blur.addListener(this, &ui::onBlurChanged);
         
-        gui.add(mix.set("mix", 0, -1, 1));
+        mainOptionsPanel.add(mix.set("mix", 0, -1, 1));
         mix.addListener(this, &ui::onMixChanged);
     }
     
@@ -56,7 +65,9 @@ public:
         mix.set(this->state->mixer->mix);
         
         // draw
-        gui.draw();
+        previewsPanel.draw();
+        audiAndFFTPanel.draw();
+        mainOptionsPanel.draw();
     }
     
     void onBlurChanged(float& value) {
