162f4e01edad10ef21161640b708f4e8eebddee9
Daniel Almeida <danielf.almeida@gmail.com>
2025-07-17 17:34:13 +0100

    chore: fixes and working keys

---
+++ engine/src/LayerStack.cpp
@@ -0,0 +1,8 @@
+//
+//  LayerStack.cpp
+//  Hello World
+//
+//  Created by Daniel Almeida on 14/07/2025.
+//
+
+#include <stdio.h>
---
+++ engine/src/LayerStack.hpp
@@ -0,0 +1,12 @@
+//
+//  LayerStack.hpp
+//  VisualsEngine
+//
+//  Created by Daniel Almeida on 14/07/2025.
+//
+
+#ifndef LayerStack_h
+#define LayerStack_h
+
+
+#endif /* LayerStack_h */
